68a289f47acae778af697f09487160c69f11e218
Ben Andrews <benjama48@hotmail.com>
2017-04-24 16:12:22 -0600

    Created a singleton of the data model

---
+++ ProjectFinal/TaskView.swift
@@ -0,0 +1,13 @@
+//
+//  TaskView.swift
+//  ProjectFinal
+//
+//  Created by Ben Andrews on 4/24/17.
+//  Copyright © 2017 John Young. All rights reserved.
+//
+
+import UIKit
+
+class TaskView: UIView {
+    
+}
